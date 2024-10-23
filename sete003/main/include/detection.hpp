5ad91bf12adc08703291a36604320f45e3cf427d
Victor <victor.alicino@gmail.com>
2024-10-23 20:24:22 -0300

    Adding Detection Class - not ready yet

--- sete003/main/include/detection.hpp
+++ sete003/main/include/detection.hpp
@@ -1,1 +1,66 @@
+
+/*
+D0--------------------------------------D3    D0--------D3
+|                                        |    |          |
+|                                        |    |          |
+|               Horizontal               |    | Vertical |
+|                                        |    |          |
+|                                        |    |          |
+D1--------------------------------------D2    D1--------D2
+*/
+
 #pragma once
+
+#include "ld2461.hpp"
+#include "mqtt.hpp"
+#include "sensor.hpp"
+
+typedef struct point{
+    float x;
+    float y;
+}point_t;
+
+typedef point vec2_t;
+
+typedef struct detection_area{
+    /*
+    D0: Top left corner
+    D1: Bottom left corner
+    D2: Bottom right corner
+    D3: Top right corner
+
+    F0: D0-D1
+    F1: D1-D2
+    F2: D2-D3
+    F3: D3-D0
+    */
+    point_t D[4]; // Points of the detection area
+    vec2_t F[4]; // Vectors of detection area faces
+}detection_area_t;
+
+class Detection{
+private:
+    detection_area_t detection_area;
+    point_t targets_previous[MAX_TARGETS_DETECTION];
+    point_t targets_current[MAX_TARGETS_DETECTION];
+
+    int entered_detections;
+    int exited_detections;
+    int gave_up_detections;
+
+    bool _pre_calc_vector_product(
+        uint8_t vecAB_index,
+        uint8_t pointA_index,
+        point_t pointC);
+    bool _is_target_in_detection_area(point_t target);
+
+public:
+    void set_detection_area(
+        point_t D0,
+        point_t D1,
+        point_t D2,
+        point_t D3
+    );
+
+
+};
--- sete003/main/src/detection.cpp
+++ sete003/main/src/detection.cpp
@@ -1,1 +1,42 @@
 #include "detection.hpp"
+
+#define POINT_D0 0
+#define POINT_D1 1
+#define POINT_D2 2
+#define POINT_D3 3
+#define VEC_D0D1 0
+#define VEC_D1D2 1
+#define VEC_D2D3 2
+#define VEC_D3D4 3
+ 
+bool Detection::_pre_calc_vector_product(uint8_t vecAB_index, uint8_t pointA_index, point_t pointC)
+{
+    return (detection_area.F[vecAB_index].x * (pointC.y - detection_area.D[pointA_index].y) -
+            detection_area.F[vecAB_index].y * (pointC.x - detection_area.D[pointA_index].x)) >= 0;
+}
+
+bool Detection::_is_target_in_detection_area(point_t target)
+{
+    return _pre_calc_vector_product(VEC_D0D1, POINT_D0, target) &&
+           _pre_calc_vector_product(VEC_D1D2, POINT_D1, target) &&
+           _pre_calc_vector_product(VEC_D2D3, POINT_D2, target) &&
+           _pre_calc_vector_product(VEC_D3D4, POINT_D3, target);
+}
+
+void Detection::set_detection_area(
+        point_t D0,
+        point_t D1,
+        point_t D2,
+        point_t D3
+    )
+{
+    detection_area.D[0] = D0;
+    detection_area.D[1] = D1;
+    detection_area.D[2] = D2;
+    detection_area.D[3] = D3;
+
+    detection_area.F[0] = {D1.x - D0.x, D1.y - D0.y};
+    detection_area.F[1] = {D2.x - D1.x, D2.y - D1.y};
+    detection_area.F[2] = {D3.x - D2.x, D3.y - D2.y};
+    detection_area.F[3] = {D0.x - D3.x, D0.y - D3.y};
+}
